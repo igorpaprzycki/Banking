497513598fbfc8c88c9ce1819ee66a424bf31731
igypap <igypap@gmail.com>
2016-10-28 23:12:05 +0200

    main method of Main.class get additional testing message at the end of the file.

---
+++ src/com/igypap/Main.java
@@ -0,0 +1,32 @@
+package com.igypap;
+
+public class Main {
+
+    public static void main(String[] args){
+        Bank bank = new Bank("PKO");
+        bank.addBranch("Wawa");
+        bank.addCustomer("Wawa", "Mike", 175.34);
+        bank.addCustomer("Wawa", "Tim", 50.05);
+        bank.addCustomer("Wawa", "Percy", 220.12);
+
+        bank.addBranch("Kielce");
+        bank.addCustomer("Kielce", "Bob", 150.54);
+
+        bank.addCustomerTransaction("Wawa", "Mike", 44.22);
+        bank.addCustomerTransaction("Wawa", "Tim", 12.44);
+        bank.addCustomerTransaction("Wawa", "Percy", 1.65);
+
+        bank.listCustomers("Wawa",true);
+        bank.listCustomers("Kielce",true);
+
+        bank.addBranch("Melbourne");
+
+        if(!bank.addCustomer("Melbourne","Brian",5.53)){
+            System.out.println("Error Melbourne branch does not exist");
+            System.out.println("Generated only for GIT revision purposes");
+        }
+
+
+
+    }
+}
